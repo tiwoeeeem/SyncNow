e0fd63c1553cb8bfe0f86ee3001eac59c96b9baf
Meet Parmar <parmarmeet166@gmail.com>
2025-07-18 11:39:21 +0530

    debug

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,7 +36,11 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-
+else{
+  app.get("/", (req, res) => {
+  res.send("API is running");
+});
+}
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
